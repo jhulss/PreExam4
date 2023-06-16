d7036b6e044cfdc423852b4e70ea2faaa8e7e4d1
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-16 16:38:59 -0400

    retorna buzz si el numero es multiplo de 5 y fizzbuzz si es multiplo de 3 y 5

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -7,9 +7,21 @@ describe("FizzBuzz", () => {
     expect(fizz.cicloFizz(3)).toEqual("Fizz");
   });
 
-  it("Deberia retornar el numero ingresado si no es multiplo de 3", () => {
+  it("Deberia retornar buzz si el numero es multiplo de 5", () => {
+    const fizz = new FizzBuzz();
+    expect(fizz.cicloFizz(5)).toEqual("Buzz");
+  });
+
+  it("Deberia retornar fizzbuzz si el numero es multiplo de 3 y 5", () => {
+    const fizz = new FizzBuzz();
+    expect(fizz.cicloFizz(15)).toEqual("FizzBuzz");
+  });
+
+  it("Deberia retornar el numero ingresado si no es multiplo de 3 o 5", () => {
     const fizz = new FizzBuzz();
     expect(fizz.cicloFizz(4)).toEqual(4);
   });
+
+ 
 
 });
